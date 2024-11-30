b4ffc0a4431e1750ddd562151da4c60896cc2253
Riya <149504570+Riyakhilari@users.noreply.github.com>
2024-12-01 00:35:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	cons lowerStr = str.toLowerCase();
+  const lowerSubStr = subStr.toLowerCase();
+ return lowerStr.indexOf(lowerSubStr);	
 }
+
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
